054a47b5ef37c60aff939edd373e902ea00fe63c
yotubarail <47407052+yotubarail@users.noreply.github.com>
2020-04-13 16:25:54 +0900

    Add files via upload

---
+++ AdTest/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  AdTest
+//
+//  Created by 滝浪翔太 on 2020/04/13.
+//  Copyright © 2020 滝浪翔太. All rights reserved.
+//
+
+import UIKit
+import GoogleMobileAds
+
+class ViewController: UIViewController, GADBannerViewDelegate {
+    
+    var bannerView: GADBannerView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bannerView = GADBannerView(adSize: kGADAdSizeBanner)
+        addBannerViewToView(bannerView)
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        bannerView.delegate = self
+    }
+
+    func addBannerViewToView(_ bannerView: GADBannerView) {
+     bannerView.translatesAutoresizingMaskIntoConstraints = false
+     view.addSubview(bannerView)
+     view.addConstraints(
+       [NSLayoutConstraint(item: bannerView,
+                           attribute: .bottom,
+                           relatedBy: .equal,
+                           toItem: bottomLayoutGuide,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 0),
+        NSLayoutConstraint(item: bannerView,
+                           attribute: .centerX,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .centerX,
+                           multiplier: 1,
+                           constant: 0)
+       ])
+    }
+
+}
+
